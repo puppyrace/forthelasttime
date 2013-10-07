6dab1dca1fbb3eaf15a278b341daabe5a7abafc6
Snow Magician <snowmagician@live.com>
2013-10-07 18:30:40 -0400

    .

--- a.b.c3/www/js/index.js
+++ a.b.c3/www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		alert("onDeviceReady function ran");
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -43,7 +44,7 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+		alert("receivedEvent function ran");
         console.log('Received Event: ' + id);
     }
 };
